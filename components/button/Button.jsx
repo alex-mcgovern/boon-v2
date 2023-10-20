f2df571143d47329ec833e2194f87f0f521f3a94
alex-mcgovern <alex_mcgovernsmith@icloud.com>
2023-10-20 09:53:43 +0100

    feat: basic button component with css modules

---
+++ components/button/Button.jsx
@@ -0,0 +1,57 @@
+import PropTypes from 'prop-types';
+import styles from './Button.module.css';
+
+/**
+* @typedef {import('react').ButtonHTMLAttributes<HTMLButtonElement>} ButtonProps
+* @property {React.ReactNode} [slotLeft] - React node to render left of the button's children
+* @property {React.ReactNode} [slotRight] - React node to render right of the button's children
+* @property {'primary' | 'secondary' | 'tertiary'} [variant] - The variant of the button
+* @property {'small' | 'medium' | 'large'} [size] - The size of the button
+*/
+
+/**
+ * A customizable button component that can render a left and/or right slot beside its children.
+ * 
+ * @param {ButtonProps} props
+ */
+export function Button(props) {
+    const { slotLeft, slotRight, children, variant = "primary", size, ...rest } = props;
+
+    const cn = [
+        styles.btn,
+        variant && styles[variant],
+        size && styles[size],
+    ].join(' ');
+
+    // const slotLeftClasses = [
+    //     styles.slotLeft,
+    //     variant && styles[`${variant}SlotLeft`],
+    //     size && styles[`${size}SlotLeft`],
+    // ].join(' ');
+
+    // const slotRightClasses = [
+    //     styles.slotRight,
+    //     variant && styles[`${variant}SlotRight`],
+    //     size && styles[`${size}SlotRight`],
+    // ].join(' ');
+
+    return (
+        <button 
+        className={cn}
+         {...rest}>
+            {/* {slotLeft && <div className={slotLeftClasses}>{slotLeft}</div>} */}
+            {children}
+            {/* {slotRight && <div className={slotRightClasses}>{slotRight}</div>} */}
+        </button>
+    );
+}
+
+// Button.propTypes = {
+//     slotLeft: PropTypes.node,
+//     slotRight: PropTypes.node,
+//     variant: PropTypes.oneOf(['primary', 'secondary', 'tertiary']),
+//     size: PropTypes.oneOf(['small', 'medium', 'large']),
+//     ...Button.propTypes,
+// };
+
+
