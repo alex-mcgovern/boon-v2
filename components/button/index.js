6823df6aa0f8b73afc0be09446104fe530a6d856
alex-mcgovern <alex_mcgovernsmith@icloud.com>
2023-10-25 17:40:45 +0100

    simplify button

---
+++ components/button/index.js
@@ -0,0 +1,34 @@
+import * as React from "react";
+
+/**
+ * @typedef {import('react').ButtonHTMLAttributes<HTMLButtonElement>} ButtonProps
+ * @property {React.ReactNode} [slotLeft] - React node to render left of the button's children
+ * @property {React.ReactNode} [slotRight] - React node to render right of the button's children
+ * @property {'primary' | 'secondary' | 'tertiary'} [variant] - The variant of the button
+ * @property {'small' | 'medium' | 'large'} [size] - The size of the button
+ * @property {'button' | 'submit'} [type] - The type of the button
+ */
+
+/**
+ * A customizable button component that can render a left and/or right slot beside its children.
+ *
+ * @param {ButtonProps} props
+ */
+export function Button(props) {
+	const {
+		slotLeft,
+		slotRight,
+		children,
+		variant = "primary",
+		size,
+		type,
+		...rest
+	} = props;
+
+	return React.jsx("button", {
+		type,
+		children,
+		className: "button",
+		...rest,
+	});
+}
