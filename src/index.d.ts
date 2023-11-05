41aa1b8864e09ce2da52ef373852e98631554480
alex-mcgovern <alex_mcgovernsmith@icloud.com>
2023-11-05 17:32:45 +0000

    improve: type defs

---
+++ src/index.d.ts
@@ -0,0 +1,9 @@
+export declare const Button: (props: {
+	slotLeft?: React.ReactNode;
+	slotRight?: React.ReactNode;
+	size: "sm" | "lg" | "square";
+	theme: "red" | "blue" | "green" | "grey" | "amber";
+	children: React.ReactNode;
+	disabled?: boolean;
+	onClick?: () => void;
+}) => JSX.Element;
