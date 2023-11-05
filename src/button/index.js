a37f4bbd180d9d7859f559bff1d3b6e3272afa74
alex-mcgovern <alex_mcgovernsmith@icloud.com>
2023-11-05 17:38:21 +0000

    improve: type defs

--- src/button/index.js
+++ src/button/index.js
@@ -3,17 +3,6 @@
 // @ts-expect-error
 import { jsx } from "react/jsx-runtime";
 
-/**
- * A customizable button component that can render a left and/or right slot beside its children.
- *
- * @param {Object} props
- * @property {React.ReactNode} [props.slotLeft] - React node to render left of the button's children
- * @property {React.ReactNode} [props.slotRight] - React node to render right of the button's children
- * @property {'primary' | 'secondary' | 'ghost'} [props.variant] - The variant of the button
- * @property {'sm' | 'lg' | 'square'} [props.size] - The size of the button
- * @property {'button' | 'submit'} [props.type] - The type of the button
- * @property {'red' | 'green' | 'default' | 'amber' | 'blue' | 'grey'} [theme] - The color theme to apply to the button
- */
 export function Button(props) {
 	const { children, theme, variant = "primary", size, type, ...rest } = props;
 
